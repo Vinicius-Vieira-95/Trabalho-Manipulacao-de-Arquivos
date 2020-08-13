a31824491ebb321466708a018ef74cc186f15669
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2020-08-13 10:17:06 -0300

    adição de arquivos dentros de pastas

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,49 +1,57 @@
 package aplication;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
-	
+
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-		
 		System.out.println("1 - Criar respostas dos alunos");
 		System.out.println("2 - Gerar Gabarito");
-		System.out.println("3 - ");
+		System.out.println("3 - Visualizar pastas das diciplinas e gabaritos");
 		System.out.println("4 - ");
 		Integer n = sc.nextInt();
-		
-		switch(n) {
-		
+
+		switch (n) {
+
 		case 1:
-			
+
 			System.out.println("Coloque o nome da disciplina");
 			String nome = sc.next();
-			
+			pastas(nome);
 			arquivo(nome);
 			break;
-		
+
 		case 2:
 			System.out.println("Coloque o nome da disciplina para gerar gabarito");
 			String gab = sc.next();
+			pastas(gab+"_Gabarito");
 			gabarito(gab);
+
+			break;
+
+		case 3:
+			System.out.println("");
+			String past = sc.next();
 			
 			break;
-		
+			
 		}
-		
+
 		sc.close();
 	}
 
 	public static void arquivo(String arquivo) {
+
 		
-		boolean flag = true; //bandeira
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\" + arquivo + ".txt"))) {
+		boolean flag = true; // bandeira
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\"+arquivo+"\\" + arquivo + ".txt"))) {
 
 			while (flag == true) {
 
@@ -68,34 +76,40 @@ public class Program {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		sc.close();
 	}
-	
+
 	public static void gabarito(String gab) {
-	
-		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\" + gab + ".txt"))) {
-			
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\" +gab+"_Gabarito\\" + gab + ".txt"))) {
+
 			System.out.print("Digite suas respostas:");
 			String res = sc.next();
 			bw.write(res);
 			bw.newLine();
 			bw.close();
-			
-		}
-		catch(IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	public static boolean pastas(String pasta) {
+		
+		boolean file = new File("c:\\temp\\"+pasta).mkdir();
+		
+		return file;
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
